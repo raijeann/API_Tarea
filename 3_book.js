3c4287eb8610a65f3464c2bf3e239fad49ae225e
raijean <rainierojean@gmail.com>
2024-02-19 17:51:19 -0700

    swapi missing

--- 3_book.js
+++ 3_book.js
@@ -2,27 +2,30 @@ async function getBookDetails(query) {
     const url = `https://openlibrary.org/search.json?q=${query}`;
 
     try {
-        const response = await fetch(url);
+        const res = await fetch(url);
 
-        if(response.ok) {
-            const bookData = await response.json();
-            const bookDetails = {
-                name: bookData.docs.map(titles => titles.title),
-            };
-
-            return bookDetails;
-        } else {
-            console.error(`Error: ${response.status}`);
-            return null;
+        if(!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
         }
+
+        const bookData = await res.json();
+        const bookDetails = {
+            titles: bookData.docs.map(doc => doc.title),
+        }
+
+        return bookDetails;
     } catch(error) {
-        console.error(`An error ocurred: ${error}`);
+        console.error(`Error: ${error.message}`);
         return null;
     }
 }
 
-getBookDetails("douglas adams").then(bookDetails => {
-    if(bookDetails) {
-        console.log(`Titles: ${bookDetails.name}`);
-    }
-})
+getBookDetails("douglas adams")
+    .then(bookDetails => {
+        if(bookDetails) {
+        console.log(`Titles:\n${bookDetails.titles.join('\n')}`);
+        }
+    })
+    .catch(error => {
+        console.error(`Error fetch ing book details: ${error.message}`);
+    })
---
+++ 5_6_7_8_swapi.js
@@ -0,0 +1,8 @@
+// 5.- Hacer una petición a la swapi a un personaje y obtener su primera película
+
+// 6.- Hacer una petición a la swapi a un personaje y obtener sus primera películas
+
+// 7.- Hacer una petición a la swapi a una película y obtener sus personajes
+
+// 8.- Hacer una petición a la swapi a un planeta y obtener los nombres de los habitantes
+
